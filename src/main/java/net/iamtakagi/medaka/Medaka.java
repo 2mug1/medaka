2f0a9202396b249c52f1ea70a09da5487804c949
takagi <iam.takagi.dev@gmail.com>
2022-09-23 20:12:09 +0900

    feat: メニュー更新タスク (BukkitTask menuUpdateTask) を静的変数として置いておく

--- src/main/java/net/iamtakagi/medaka/Medaka.java
+++ src/main/java/net/iamtakagi/medaka/Medaka.java
@@ -2,6 +2,7 @@ package net.iamtakagi.medaka;
 
 import lombok.Getter;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitTask;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -20,6 +21,13 @@ public final class Medaka {
     private final static Map<UUID, Menu> currentlyOpenedMenus = new HashMap<>();
 
     /**
+     * メニュー更新タスク
+     * なんかのときに使えそうなので、一応変数化して置いておく
+     */
+    @Getter
+    private static BukkitTask menuUpdateTask;
+
+    /**
      * 初期化処理を行う関数
      * 外部から Medaka.init(org.bukkit.plugin.Plugin); を呼び出すことで処理される
      * @param plugin
@@ -27,7 +35,7 @@ public final class Medaka {
     public static void init (Plugin plugin) {
         plugin.getLogger().info("[medaka] 初期化中...");
         plugin.getLogger().info("[medaka] MenuUpdateRunnable を起動しています");
-        plugin.getServer().getScheduler().runTaskTimer(plugin, new MenuUpdateRunnable(), 20L, 20L);
+        menuUpdateTask = plugin.getServer().getScheduler().runTaskTimer(plugin, new MenuUpdateRunnable(), 20L, 20L);
         plugin.getLogger().info("[medaka] MenuUpdateRunnable を起動しました");
         plugin.getLogger().info("[medaka] 初期化が完了しました");
     }
